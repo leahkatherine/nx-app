fe1226688df711ecef157bf494dffa3e8d808809
Leah Anestos <leahkanestos@gmail.com>
2024-01-11 21:30:46 -0500

    Card.tsx created

---
+++ libs/web/design-system/src/lib/core/Card/Card.tsx
@@ -0,0 +1,7 @@
+export { Card, CardActionArea, CardActions, CardContent, CardHeader, CardMedia } from '@mui/material'
+export type {
+  CardProps,
+  CardActionAreaProps,
+  CardActionsProps,
+  CardContentProps,
+} from '@mui/material'
